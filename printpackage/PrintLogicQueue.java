3caf70db2d801aa5cee8bb37f085f222428ee1d9
shivangi-engg <62798764+shivangi-engg@users.noreply.github.com>
2020-12-26 16:04:59 +0530

    first commit

--- printpackage/PrintLogicQueue.java
+++ printpackage/PrintLogicQueue.java
@@ -13,7 +13,9 @@ public class PrintLogicQueue
 		System.out.println("enter the length of array:");
 		int length = sc.nextInt();
 		int last = length-1;
+		//array
 		int arr[] = new int[length];
+		//for loop to take the elements for array
 		for (int j=0;j<=last ;j++ ) 
 		{
 			System.out.println("enter the"+j+"th element of array:");
@@ -21,16 +23,19 @@ public class PrintLogicQueue
 			int e = sa.nextInt();
 			arr[j] = e;
 		}
+		//for loop to traverse from starting to ending of array
 		for (i=0;i<=last ;i++ ) 
 		{
+			//if ith element is 0 goto next element
 			if (arr[i] == 0) 
 			{
 				continue;
 			}	
+			//if ith element is not 0 pop it
 			else
 			{
 				System.out.print("the popped element is:"+arr[i]);
-
+				//to pop the element
 				arr[i] = 0;
 				break;
 			}
@@ -46,7 +51,9 @@ public class PrintLogicQueue
 		System.out.println("enter the length of array:");
 		int length = sc.nextInt();
 		int last = length-1;
+		//array
 		int arr[] = new int[length];
+		//for loop to take values of element for array
 		for (int j=0;j<=last ;j++ ) 
 		{
 			System.out.println("enter the"+j+"th element of array:");
@@ -54,15 +61,19 @@ public class PrintLogicQueue
 			int e = sa.nextInt();
 			arr[j] = e;
 		}
+		//take input from user , i.e the value u want to push
 		Scanner se =new Scanner(System.in);
 		System.out.println("enter the element u want to enter in the array:");
 		int element = se.nextInt();
+		//for loop to traverse from starting to ending of array
 		for (i=0;i<=last ;i++ ) 
 		{
+			//if i is the last value of array put the value of element in it
 			if (arr[i] == arr[last]) 
 			{
 				arr[i]=element;
 			}
+			//for not last elements, put the value of next index at this index
 			else
 			{
 				int temp;
@@ -73,6 +84,7 @@ public class PrintLogicQueue
 			
 		}
 		System.out.println("the array after performing the push operation is:");
+		//for loop to give the final array after push operation
 		for (int k=0;k<=last;k++) 
 		{
 			System.out.println(arr[k]);
