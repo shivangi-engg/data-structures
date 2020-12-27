9638d6f4d20a2353729260d153a49eb51275aa3c
shivangi-engg <62798764+shivangi-engg@users.noreply.github.com>
2020-12-27 12:58:37 +0530

    second commit

--- Main.java
+++ Main.java
@@ -6,24 +6,53 @@ public class Main
 	{
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter 1 for pop , 2 for push");
+		System.out.println("enter 1 for queue , 2 for stack");
 		int s = sc.nextInt();
 		switch(s)
 		{
 			case 1:
-				printpackage.PrintLogicQueue printLogicQueueInstance = new  printpackage.PrintLogicQueue();
-				printLogicQueueInstance.printLogicPop();
-				break;
+				Scanner sa = new Scanner(System.in);
+				System.out.println("enter 1 for pop  and 2 for push");
+				int p = sa.nextInt();
+				switch(p)
+				{
+					case 1:
+						printpackage.PrintLogicQueue printLogicQueueInstance = new  printpackage.PrintLogicQueue();
+						printLogicQueueInstance.printLogicPop();
+						break;
 
+					case 2:
+						printpackage.PrintLogicQueue printLogicQueueInstance1 = new printpackage.PrintLogicQueue();
+						printLogicQueueInstance1.printLogicPush();
+						break;
+
+
+					default:
+						System.out.println("that is not an option");
+				}
 
 			case 2:
-				printpackage.PrintLogicQueue printLogicQueueInstance1 = new printpackage.PrintLogicQueue();
-				printLogicQueueInstance1.printLogicPush();
-				break;
+				Scanner sw = new Scanner(System.in);
+				System.out.println("enter 1 for pop  and 2 for push");
+				int r = sw.nextInt();
+				switch(r)
+				{
+					case 1:
+						printpackage.PrintLogicStack printLogicStackInstance = new printpackage.PrintLogicStack();
+						printLogicStackInstance.printLogicPop();
+
+					case 2:
+						printpackage.PrintLogicStack printLogicStackInstance1 = new printpackage.PrintLogicStack();
+						printLogicStackInstance1.printLogicPush();
+						break;
+
+					default:
+						System.out.println("wrong option");
+				}
+				
 
 
-			default:
-				System.out.println("that is not an option");
+			
 		}
 }
 }
---
+++ printpackage/PrintLogicStack.java
@@ -0,0 +1,88 @@
+package printpackage;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class PrintLogicStack
+{
+	public void printLogicPop()
+	{
+		int i;
+		//to take length of array
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the length of array:");
+		int length = sc.nextInt();
+		int last = length-1;
+		//array
+		int arr[] = new int[length];
+		//for loop to take the elements for array
+		for (int j=0;j<=last ;j++ ) 
+		{
+			System.out.println("enter the"+j+"th element of array:");
+			Scanner sa = new Scanner(System.in);
+			int e = sa.nextInt();
+			arr[j] = e;
+		}
+		//for loop to traverse from starting to ending of array
+		for (i=0;i<=last ;i++ ) 
+		{
+			//if ith element is 0 goto next element
+			if (arr[i] == 0) 
+			{
+				continue;
+			}	
+			//if ith element is not 0 pop it
+			else
+			{
+				System.out.println("the popped element is:"+arr[i]);
+				//to pop the element
+				arr[i] = 0;
+				break;
+			}
+		}
+
+	}
+	public void printLogicPush()
+	{
+		int i;
+		//to take length of array
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the length of array:");
+		int length = sc.nextInt();
+		int last = length-1;
+		//array
+		int arr[] = new int[length];
+		//for loop to take values of element for array
+		for (int j=0;j<=last ;j++ ) 
+		{
+			System.out.println("enter the"+j+"th element of array:");
+			Scanner sa = new Scanner(System.in);
+			int e = sa.nextInt();
+			arr[j] = e;
+		}
+		//take input from user , i.e the value u want to push
+		Scanner se =new Scanner(System.in);
+		System.out.println("enter the element u want to enter in the array:");
+		int element = se.nextInt();
+		//for loop to traverse from starting to ending of array
+		for (i=last;i>=0 ;i-- ) 
+		{
+			if (arr[i] == arr[0]) 
+			{
+				arr[i] = element;
+			}
+			else
+			{
+				int temp;
+				temp = arr[i-1];
+				arr[i] = temp;
+			}
+			
+		}
+		System.out.println("the array after performing the push operation is:");
+		//for loop to give the final array after push operation
+		for (int k=0;k<=last;k++) 
+		{
+			System.out.println(arr[k]);
+		}
+	}
+}
